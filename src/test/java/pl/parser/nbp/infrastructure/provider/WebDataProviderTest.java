024e35c889cc0404f57d0c25aa18db9bf2e851e7
nikom1337 <kornel.rabczak@gmail.com>
2016-07-31 19:41:06 +0200

    removed flaky test

--- src/test/java/pl/parser/nbp/infrastructure/provider/WebDataProviderTest.java
+++ src/test/java/pl/parser/nbp/infrastructure/provider/WebDataProviderTest.java
@@ -6,7 +6,6 @@ import pl.parser.nbp.bussiness.currency.entity.CurrencyRequest;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -21,18 +20,6 @@ public class WebDataProviderTest {
     public void should_fail_for_malformed_url() throws IOException {
         // given
         String url = "qqwe.aszx:qwe";
-        when(request.getSource()).thenReturn(url);
-
-        // when
-        try(InputStream ignored = sut.fetch()) {
-
-        }
-    }
-
-    @Test(expected = SocketTimeoutException.class)
-    public void should_timeout() throws IOException {
-        // given
-        String url = "http://localhost:12345/index.php";
         when(request.getSource()).thenReturn(url);
 
         // when
